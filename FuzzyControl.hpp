d162300d9b591bb53c115a8d018064d7e4146709
Kevin Riggs <riggskevinp@gmail.com>
2022-12-01 18:56:40 -0600

    Add inverted pendulum unit test.

--- FuzzyControl.hpp
+++ FuzzyControl.hpp
@@ -2,17 +2,29 @@
 #include <algorithm>
 #include <iostream>
 #include <memory>
+#include <utility>
+#include <numeric>
+
+template <typename T>
+struct fail : std::false_type
+        {
+        };
 
 class MembershipFunction{
 public:
-    virtual double operator()(double _x) = 0;
+    virtual double getValue(double _x) = 0;
+    double operator()(double _x) {return getValue(_x);}
+    virtual double center() = 0;
+    virtual double area() {return 0;};
     virtual ~MembershipFunction() = default;
 };
 
 class TriangleMF: public MembershipFunction{
 public:
     TriangleMF(double _a, double _b, double _c): a(_a),b(_b),c(_c){}
-    double operator()(double _x) override {return std::max(std::min((_x-a)/(b-a), (c-_x)/(c-b)),0.0);}
+    double getValue(double _x) override {return std::max(std::min((_x-a)/(b-a), (c-_x)/(c-b)),0.0);}
+    double center() override {return (a+c)/2;}
+    double area() override {return (c-a)/2;}
 
 private:
     double a;
@@ -23,7 +35,10 @@ private:
 class TrapezoidalMF: public MembershipFunction{
 public:
     TrapezoidalMF(double _a, double _b, double _c, double _d): a(_a),b(_b),c(_c),d(_d){}
-    double operator()(double _x) override {return std::max(std::min({(_x-a)/(b-a), 1.0, (d-_x)/(d-c)}),0.0);}
+    double getValue(double _x) override {return std::max(std::min({(_x-a)/(b-a), 1.0, (d-_x)/(d-c)}),0.0);}
+    double center() override {return (a+d)/2;}
+    double area() override {return (a+d + b+c)/2;}
+
 
 private:
     double a;
@@ -35,7 +50,10 @@ private:
 class GaussianMF: public MembershipFunction{
 public:
     GaussianMF(double _c, double _sigma): c(_c),sigma(_sigma){}
-    double operator()(double _x) override {return exp(-.5*pow((_x-c)/sigma,2));}
+    double getValue(double _x) override {return exp(-.5*pow((_x-c)/sigma,2));}
+    double center() override {return c;}
+    template<typename T = bool>
+    double area() {static_assert (fail<T>::value, "Not implemented");}
 
 private:
     double c;
@@ -45,7 +63,10 @@ private:
 class GeneralizedBellMF: public MembershipFunction{
 public:
     GeneralizedBellMF(double _a, double _b, double _c): a(_a),b(_b),c(_c){}
-    double operator()(double _x) override {return pow(1+pow(abs((_x-c)/a),2*b),-1);}
+    double getValue(double _x) override {return pow(1+pow(abs((_x-c)/a),2*b),-1);}
+    double center() override {return c;}
+    template<typename T = bool>
+    double area() {static_assert (fail<T>::value, "Not implemented");}
 
 private:
     double a;
@@ -56,7 +77,10 @@ private:
 class SigmoidalMF: public MembershipFunction{
 public:
     SigmoidalMF(double _a, double _c): a(_a),c(_c){}
-    double operator()(double _x) override {return 1 / (1+exp(-a*(_x-c)));}
+    double getValue(double _x) override {return 1 / (1+exp(-a*(_x-c)));}
+    double center() override {return c;}
+    template<typename T = bool>
+    double area() {static_assert (fail<T>::value, "Not implemented");}
 
 private:
     double a;
@@ -66,8 +90,95 @@ private:
 
 class FuzzySet{
 public:
-    FuzzySet(MembershipFunction* _mf):mf(_mf){}
+    explicit FuzzySet(MembershipFunction* _mf):mf(_mf){}
+    double operator()(double _x) {return mf->getValue(_x);}
+    double center() {return mf->center();}
+    double area() {return mf->area();}
+    void fuzzify(double crispInput){fuzzValue = mf->getValue(crispInput);}
 
 private:
     std::shared_ptr<MembershipFunction> mf;
+    double fuzzValue{0.0};
+};
+
+
+class FuzzyRule{
+public:
+    explicit FuzzyRule(std::vector<FuzzySet> _in, FuzzySet _out): in(std::move(_in)),out(std::move(_out)){}
+    std::vector<double> getWeights(std::vector<double> crispIn){
+        std::vector<double> _weights;
+        _weights.reserve(crispIn.size());
+        for(int i = 0; i < crispIn.size(); i++){
+            _weights.push_back(in.at(i)(crispIn.at(i)));
+        }
+        return _weights;
+    }
+    double getCenter(){return out.center();}
+    double getArea(){return out.area();}
+private:
+    std::vector<FuzzySet> in;
+    FuzzySet out;
+};
+
+class SimplifiedIEwCA{
+public:
+    explicit SimplifiedIEwCA(std::vector<FuzzyRule> _rules):rules(std::move(_rules)){}
+
+protected:
+    std::vector<FuzzyRule> rules;
+};
+
+
+class SimplifiedPIECA: public SimplifiedIEwCA{
+public:
+    explicit SimplifiedPIECA(std::vector<FuzzyRule> _rules):SimplifiedIEwCA(std::move(_rules)){}
+
+    double fuzzDefuzz(const std::vector<double>& crispInputs){
+        double num = 0;
+        double den = 0;
+        for(auto rule : rules){
+            auto weights = rule.getWeights(crispInputs);
+            double weight = std::accumulate(weights.begin(), weights.end(), 1.0, std::multiplies<>());
+            den += weight;
+            num += weight*rule.getCenter();
+        }
+        if(den == 0){
+            return 0;
+        }
+        return num/den;
+    }
+};
+
+class SimplifiedMIECA: public SimplifiedIEwCA{
+public:
+    explicit SimplifiedMIECA(std::vector<FuzzyRule> _rules):SimplifiedIEwCA(std::move(_rules)){}
+
+    double fuzzDefuzz(const std::vector<double>& crispInputs){
+        double num = 0;
+        double den = 0;
+        for(auto rule : rules){
+            auto weights = rule.getWeights(crispInputs);
+            double weight = *(std::max_element(weights.begin(),weights.end()));
+            den += weight;
+            num += weight*rule.getCenter();
+        }
+        return num/den;
+    }
+};
+
+class SumProdIECoA: public SimplifiedIEwCA{
+public:
+    explicit SumProdIECoA(std::vector<FuzzyRule> _rules):SimplifiedIEwCA(std::move(_rules)){}
+
+    double fuzzDefuzz(const std::vector<double>& crispInputs){
+        double num = 0;
+        double den = 0;
+        for(auto rule : rules){
+            auto weights = rule.getWeights(crispInputs);
+            double weight = std::accumulate(weights.begin(), weights.end(), 1.0, std::multiplies<>());
+            den += weight*rule.getArea();
+            num += weight*rule.getArea()*rule.getCenter();
+        }
+        return num/den;
+    }
 };
--- tests/UnitTests.cpp
+++ tests/UnitTests.cpp
@@ -6,6 +6,8 @@
 #include "gmock/gmock.h"
 #include "../FuzzyControl.hpp"
 
+#include <limits>
+
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
     // Expect two strings not to be equal.
@@ -53,6 +55,165 @@ TEST(MembershipFunctions, Sigmoidal) {
     auto t_mf = SigmoidalMF(1, -5);
     EXPECT_EQ(t_mf(-5),0.5);
 }
+
+TEST(FuzzySet, Sigmoidal) {
+    auto t_mf = FuzzySet(new SigmoidalMF(1, -5));
+    t_mf.area();
+}
+
+TEST(InferenceEngines, Simplified_PIE_with_CA){
+    // Input Variable
+    auto cold = FuzzySet(new TriangleMF(-0.0001,0,30));
+    auto cool = FuzzySet(new TriangleMF(0,20,70));
+    auto warm = FuzzySet(new TriangleMF(40,70,std::numeric_limits<double>::max()));
+
+    // Output Variable
+    auto fast = FuzzySet(new TriangleMF(70,80,90));
+    auto medium = FuzzySet(new TriangleMF(50,60,70));
+    auto slow = FuzzySet(new TriangleMF(10,20,30));
+
+    auto ifTempIsColdThenFanIsFast = FuzzyRule({cold},fast);
+    auto ifTempICoolThenFanIsMedium = FuzzyRule({cool},medium);
+    auto ifTempIsWarmThenFanIsSlow = FuzzyRule({warm},slow);
+
+    SimplifiedPIECA fc({ifTempIsColdThenFanIsFast,
+                        ifTempICoolThenFanIsMedium,
+                        ifTempIsWarmThenFanIsSlow,
+    });
+
+    EXPECT_DOUBLE_EQ(fc.fuzzDefuzz({0}), 80.0);
+    EXPECT_DOUBLE_EQ(fc.fuzzDefuzz({35}), 60.0);
+    EXPECT_DOUBLE_EQ(fc.fuzzDefuzz({90}), 20.0);
+
+}
+
+TEST(InferenceEngines, Simplified_MIE_with_CA_MultiIn){
+    // Input Variable
+    auto cold = FuzzySet(new TriangleMF(std::numeric_limits<int>::min(),0,30));
+    auto cool = FuzzySet(new TriangleMF(-10,20,50));
+    auto warm = FuzzySet(new TriangleMF(10,40,std::numeric_limits<int>::max()));
+
+    // Input Variable
+    auto dark = FuzzySet(new TriangleMF(std::numeric_limits<int>::min(),10,40));
+    auto overcast = FuzzySet(new TriangleMF(10,40,70));
+    auto sunny = FuzzySet(new TriangleMF(40,70,std::numeric_limits<int>::max()));
+
+    // Output Variable
+    auto fast = FuzzySet(new TriangleMF(70,80,90));
+    auto medium = FuzzySet(new TriangleMF(50,60,70));
+    auto slow = FuzzySet(new TriangleMF(10,20,30));
+
+    auto rule1 = FuzzyRule({cold,dark},fast);
+    auto rule2 = FuzzyRule({cool,overcast},medium);
+    auto rule3 = FuzzyRule({warm,sunny},slow);
+
+    SimplifiedMIECA fc({rule1,
+                        rule2,
+                        rule3,
+                        });
+
+    /*for(int i = -5; i < 100; i++){
+        for(int j = -5; j < 100; j++){
+            std::cout << fc.fuzzDefuzz({double(i),double(j)}) << std::endl;
+        }
+    }*/
+}
+
+TEST(InferenceEngines, InvertedPendulum){
+    // Input Linguistic Variable "error"
+    auto negativeLargeError = FuzzySet(new TriangleMF(std::numeric_limits<int>::min(), -M_PI/2, -M_PI/4));
+    auto negativeSmallError = FuzzySet(new TriangleMF(-M_PI/2, -M_PI/4,0));
+    auto zeroError = FuzzySet(new TriangleMF(-M_PI/4,0, M_PI/4));
+    auto positiveSmallError = FuzzySet(new TriangleMF(0, M_PI/4, M_PI/2));
+    auto positiveLargeError = FuzzySet(new TriangleMF(M_PI/4, M_PI/2, std::numeric_limits<int>::max()));
+
+    // Input Linguistic Variable "change in error"
+    auto negativeLargeErrorDelta = FuzzySet(new TriangleMF(std::numeric_limits<int>::min(), -M_PI/2, -M_PI/4));
+    auto negativeSmallErrorDelta = FuzzySet(new TriangleMF(-M_PI/2, -M_PI/4,0));
+    auto zeroErrorDelta = FuzzySet(new TriangleMF(-M_PI/4,0, M_PI/4));
+    auto positiveSmallErrorDelta = FuzzySet(new TriangleMF(0, M_PI/4, M_PI/2));
+    auto positiveLargeErrorDelta = FuzzySet(new TriangleMF(M_PI/4, M_PI/2, std::numeric_limits<int>::max()));
+
+
+    // Output Linguistic Variable "control force"
+    auto negativeLargeControl = FuzzySet(new TriangleMF(-30, -20, -10));
+    auto negativeSmallControl = FuzzySet(new TriangleMF(-20, -10,0));
+    auto zeroControl = FuzzySet(new TriangleMF(-10,0,10));
+    auto positiveSmallControl = FuzzySet(new TriangleMF(0,10,20));
+    auto positiveLargeControl = FuzzySet(new TriangleMF(10,20,30));
+
+
+    auto rule1 = FuzzyRule({negativeLargeError,negativeLargeErrorDelta},positiveLargeControl);
+    auto rule2 = FuzzyRule({negativeLargeError,negativeSmallError},positiveLargeControl);
+    auto rule3 = FuzzyRule({negativeLargeError,zeroErrorDelta},positiveLargeControl);
+    auto rule4 = FuzzyRule({negativeLargeError,positiveSmallErrorDelta},positiveSmallControl);
+    auto rule5 = FuzzyRule({negativeLargeError,positiveLargeErrorDelta},zeroControl);
+
+    auto rule6 = FuzzyRule({negativeSmallError,negativeLargeErrorDelta},positiveLargeControl);
+    auto rule7 = FuzzyRule({negativeSmallError,negativeSmallError},positiveLargeControl);
+    auto rule8 = FuzzyRule({negativeSmallError,zeroErrorDelta},positiveSmallControl);
+    auto rule9 = FuzzyRule({negativeSmallError,positiveSmallErrorDelta},zeroControl);
+    auto rule10 = FuzzyRule({negativeSmallError,positiveLargeErrorDelta},negativeSmallControl);
+
+    auto rule11 = FuzzyRule({zeroError,negativeLargeErrorDelta},positiveLargeControl);
+    auto rule12 = FuzzyRule({zeroError,negativeSmallError},positiveSmallControl);
+    auto rule13 = FuzzyRule({zeroError,zeroErrorDelta},zeroControl);
+    auto rule14 = FuzzyRule({zeroError,positiveSmallErrorDelta},negativeSmallControl);
+    auto rule15 = FuzzyRule({zeroError,positiveLargeErrorDelta},negativeLargeControl);
+
+    auto rule16 = FuzzyRule({positiveSmallError,negativeLargeErrorDelta},positiveSmallControl);
+    auto rule17 = FuzzyRule({positiveSmallError,negativeSmallError},zeroControl);
+    auto rule18 = FuzzyRule({positiveSmallError,zeroErrorDelta},negativeSmallControl);
+    auto rule19 = FuzzyRule({positiveSmallError,positiveSmallErrorDelta},negativeLargeControl);
+    auto rule20 = FuzzyRule({positiveSmallError,positiveLargeErrorDelta},negativeLargeControl);
+
+    auto rule21 = FuzzyRule({positiveLargeError,negativeLargeErrorDelta},zeroControl);
+    auto rule22 = FuzzyRule({positiveLargeError,negativeSmallError},positiveSmallControl);
+    auto rule23 = FuzzyRule({positiveLargeError,zeroErrorDelta},negativeLargeControl);
+    auto rule24 = FuzzyRule({positiveLargeError,positiveSmallErrorDelta},negativeLargeControl);
+    auto rule25 = FuzzyRule({positiveLargeError,positiveLargeErrorDelta},negativeLargeControl);
+
+
+    SumProdIECoA fc = SumProdIECoA({
+        FuzzyRule({negativeLargeError,negativeLargeErrorDelta},positiveLargeControl),
+        FuzzyRule({negativeLargeError,negativeSmallError},positiveLargeControl),
+        FuzzyRule({negativeLargeError,zeroErrorDelta},positiveLargeControl),
+        FuzzyRule({negativeLargeError,positiveSmallErrorDelta},positiveSmallControl),
+        FuzzyRule({negativeLargeError,positiveLargeErrorDelta},zeroControl),
+
+        FuzzyRule({negativeSmallError,negativeLargeErrorDelta},positiveLargeControl),
+        FuzzyRule({negativeSmallError,negativeSmallError},positiveLargeControl),
+        FuzzyRule({negativeSmallError,zeroErrorDelta},positiveSmallControl),
+        FuzzyRule({negativeSmallError,positiveSmallErrorDelta},zeroControl),
+        FuzzyRule({negativeSmallError,positiveLargeErrorDelta},negativeSmallControl),
+
+        FuzzyRule({zeroError,negativeLargeErrorDelta},positiveLargeControl),
+        FuzzyRule({zeroError,negativeSmallError},positiveSmallControl),
+        FuzzyRule({zeroError,zeroErrorDelta},zeroControl),
+        FuzzyRule({zeroError,positiveSmallErrorDelta},negativeSmallControl),
+        FuzzyRule({zeroError,positiveLargeErrorDelta},negativeLargeControl),
+
+        FuzzyRule({positiveSmallError,negativeLargeErrorDelta},positiveSmallControl),
+        FuzzyRule({positiveSmallError,negativeSmallError},zeroControl),
+        FuzzyRule({positiveSmallError,zeroErrorDelta},negativeSmallControl),
+        FuzzyRule({positiveSmallError,positiveSmallErrorDelta},negativeLargeControl),
+        FuzzyRule({positiveSmallError,positiveLargeErrorDelta},negativeLargeControl),
+
+        FuzzyRule({positiveLargeError,negativeLargeErrorDelta},zeroControl),
+        FuzzyRule({positiveLargeError,negativeSmallError},positiveSmallControl),
+        FuzzyRule({positiveLargeError,zeroErrorDelta},negativeLargeControl),
+        FuzzyRule({positiveLargeError,positiveSmallErrorDelta},negativeLargeControl),
+        FuzzyRule({positiveLargeError,positiveLargeErrorDelta},negativeLargeControl)
+    });
+
+    for(int x = -2; x <= 2; x++){
+        for(int y = -2; y <= 2; y++){
+            std::cout << "x: " << x << " y: " << y << " ";
+            std::cout << fc.fuzzDefuzz({double(x),double(y)}) << std::endl;
+        }
+    }
+}
+
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
